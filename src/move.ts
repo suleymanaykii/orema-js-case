62a493dd3af2ef56e429929a5768ad4f31ece4cf
Aykı Ailesi <kanalimsuleyman@gmail.com>
2023-07-18 21:36:00 +0300

    Ödev 18.07.2022

--- src/move.ts
+++ src/move.ts
@@ -1,29 +1,25 @@
 // Please update this type as same as with the data shape.
 type File = { id: string; name: string };
 type List = Array<{ id: string; name: string; files: Array<File> }>;
-let sourceFile: File;
-let destinationIndex: number;
+let sourceFileData: File;
+let destinationNumber: number;
 
 export default function move(list: List, source: string, destination: string): List {
   // find file
-  // eslint-disable-next-line array-callback-return
-  list.map((item) => {
-    if (item.id === source) throw new Error('You cannot move a folder');
-    // eslint-disable-next-line array-callback-return
-    item.files.map((file, index) => {
-      if (file.id === source) {
-        sourceFile = file; // file added to temp
-        item.files.splice(index, 1); // file removed
+  list.forEach((data) => {
+    if (data.id === source) throw new Error('You cannot move a folder');
+    data.files.forEach((fileData, index) => {
+      if (fileData.id === source) {
+        sourceFileData = fileData; // file add sourceFileData
+        data.files.splice(index, 1); // File deleted
       }
     });
   });
-
   // write file
-  destinationIndex = list.findIndex((item) => item.id === destination);
-  if (destinationIndex < 0) throw new Error('You cannot specify a file as the destination');
-  if (list[destinationIndex].files.findIndex((item) => item.name === sourceFile.name) >= 0)
-    throw new Error('A folder cannot have two file with the identical name');
-  list[destinationIndex].files.push(sourceFile); // file added
-
+  destinationNumber = list.findIndex((item) => item.id === destination);
+  if (destinationNumber < 0) throw new Error('You cannot specify a file as the destination');
+  if (list[destinationNumber].files.findIndex((item) => item.name === sourceFileData.name) >= 0)
+    throw new Error('Cannot have more than one folder named file');
+  list[destinationNumber].files.push(sourceFileData); // file added
   return list;
 }
